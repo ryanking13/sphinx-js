cb5fdf25164f048f50dd4633f201375d4800b457
Hood Chatham <roberthoodchatham@gmail.com>
2023-08-26 14:33:43 +0200

    Test different typedoc versions (#38)

--- noxfile.py
+++ noxfile.py
@@ -7,3 +7,13 @@ def tests(session: Session) -> None:
     session.install("-r", "requirements_dev.txt")
     session.run("npm", "i", "--no-save", "jsdoc@4.0.0", "typedoc@0.20", external=True)
     session.run("pytest", "--junitxml=test-results.xml")
+
+
+@nox.session(python=["3.10", "3.11"])
+@nox.parametrize("typedoc", ["0.20", "0.21"])
+def test_typedoc(session: Session, typedoc: str) -> None:
+    session.install("-r", "requirements_dev.txt")
+    session.run(
+        "npm", "i", "--no-save", "jsdoc@4.0.0", f"typedoc@{typedoc}", external=True
+    )
+    session.run("pytest", "--junitxml=test-results.xml", "-k", "not js")
