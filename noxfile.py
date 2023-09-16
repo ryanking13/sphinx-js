ffb34a2c546623a830234272996431e0a7f4122f
Hood Chatham <roberthoodchatham@gmail.com>
2023-09-15 17:21:40 -0700

    Make nox install node_modules inside the nox folder (#40)
    This better isolates the generated nox environments

--- noxfile.py
+++ noxfile.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import nox
 from nox.sessions import Session
 
@@ -12,8 +14,12 @@ def tests(session: Session) -> None:
 @nox.session(python=["3.10", "3.11"])
 @nox.parametrize("typedoc", ["0.20", "0.21", "0.22"])
 def test_typedoc(session: Session, typedoc: str) -> None:
+
     session.install("-r", "requirements_dev.txt")
-    session.run(
-        "npm", "i", "--no-save", "jsdoc@4.0.0", f"typedoc@{typedoc}", external=True
-    )
+    venvroot = Path(session.bin).parent
+    (venvroot / "node_modules").mkdir()
+    with session.chdir(venvroot):
+        session.run(
+            "npm", "i", "--no-save", "jsdoc@4.0.0", f"typedoc@{typedoc}", external=True
+        )
     session.run("pytest", "--junitxml=test-results.xml", "-k", "not js")
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,19 @@
 import os
+import sys
 from pathlib import Path
 
 import pytest
 from sphinx.testing.path import path
 
 if "SPHINX_JS_NODE_MODULES" not in os.environ:
-    os.environ["SPHINX_JS_NODE_MODULES"] = str(
-        Path(__file__).parents[1] / "node_modules"
-    )
+    for p in [Path(sys.prefix), Path(__file__).parents[1]]:
+        p = p / "node_modules"
+        if p.exists():
+            os.environ["SPHINX_JS_NODE_MODULES"] = str(p)
+            break
+    else:
+        raise RuntimeError("Couldn't find node_modules")
+
 
 pytest_plugins = "sphinx.testing.fixtures"
 
